0530e6d1e1b587689f1f35e056eef8b63dbddd76
Eugene Wekesa <eugenco578@gmail.com>
2025-05-23 10:38:21 +0300

    implement cash register class

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        self.last_transaction_amount = 0
+
+    def add_item(self, title, price, quantity=1):
+        self.total += price * quantity
+        self.items.extend([title] * quantity)
+        self.last_transaction_amount = price * quantity
+
+    def apply_discount(self):
+        if self.discount > 0:
+            discount_amount = self.total * (self.discount / 100)
+            self.total -= discount_amount
+            # Convert total to int if it is a whole number
+            if float(self.total).is_integer():
+                self.total = int(self.total)
+            print(f"After the discount, the total comes to ${self.total}.")
+        else:
+            print("There is no discount to apply.")
+
+    def void_last_transaction(self):
+        self.total -= self.last_transaction_amount
+        if self.total < 0:
+            self.total = 0
